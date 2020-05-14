e1735f65f0442e6a03fbf84c4d52c691c3ee5a3e
vbartolini <vbartolini@users.noreply.github.com>
2020-05-14 23:13:50 +0200

    tests

---
+++ EmployeeManagerAppTest/EmployeeTest.cs
@@ -0,0 +1,18 @@
+﻿using EmployeeManagerApp.Mock;
+using Xunit;
+using System.Linq;
+
+namespace EmployeeManagerAppTest
+{
+    public class EmployeeTest
+    {
+        [Fact]
+        public void Is_Employee_Deleted()
+        {
+            var repo = new EmployeeRepository();
+            var employee = repo.EmployeeList.SingleOrDefault(x => x.IsDeleted == false);
+            employee.Delete();
+            Assert.True(employee.IsDeleted);
+        }
+    }
+}
